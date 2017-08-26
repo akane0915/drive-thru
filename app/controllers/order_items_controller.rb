ff977713b64067feee0ea0c3d863946acae24ac1
akane0915 <asialkane@gmail.com>
2017-08-26 08:14:12 -0700

    create products and order items controller'

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,37 @@
+class OrderItemsController < ApplicationController
+
+  def create
+    @order = current_order
+    @item = @order.order_items.new(item_params)
+    @order.save
+    session[:order_id] = @order.id
+    respond_to do |format|
+        format.html { redirect_to products_path }
+        format.js
+      end
+  end
+
+  def update
+    @order = current_order
+    @item = @order.order_items.find(params[:id])
+    @item.update_attributes(item_params)
+    @order.save
+  end
+
+  def destroy
+    @order = current_order
+    @item = @order.order_items.find(params[:id])
+    @item.destroy
+    @order.save
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:order_item).permit(:product_id)
+  end
+end

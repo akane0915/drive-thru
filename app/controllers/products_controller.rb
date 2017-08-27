63755961bdafe9cfd3ecd8d76c3ecfd27ab6d89e
akane0915 <asialkane@gmail.com>
2017-08-27 13:14:28 -0700

    add orders functionality to alert if open orders exceed 4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
     @order_items = current_order.order_items
     @order_item = current_order.order_items.new
     @orders = Order.all
+    @open_order_count = Order.where(:status => "Open").count
     @order = current_order
   end
 end

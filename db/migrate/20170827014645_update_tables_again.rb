edce611b93d43bb36932d74979e74f3f1d8e6df9
akane0915 <asialkane@gmail.com>
2017-08-27 18:04:46 -0700

    update migrations

--- db/migrate/20170827014645_update_tables_again.rb
+++ db/migrate/20170827014645_update_tables_again.rb
@@ -1,7 +1,5 @@
 class UpdateTablesAgain < ActiveRecord::Migration[5.1]
   def change
-    add_column :order_items, :quantity, :integer
-    add_column :orders, :status, :integer
-    add_column :orders, :total_price, :decimal
+
   end
 end
